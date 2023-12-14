6745391699f27b98a339f47b06d48ef4b24d55cb
nicojl <njl27@hotmail.com>
2023-12-13 20:41:28 -0600

    First commit

---
+++ Class1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace SqlEasyLib
+{
+	public static class SqlEasy
+	{
+    /// <summary>
+    /// Return a SqlConnectionStringBuilder to alow to create a SqlConnection.
+    /// </summary>
+    /// <param name="dataSource">Server Name</param>
+    /// <param name="user">UserId</param>
+    /// <param name="pass">Password</param>
+    /// <param name="db">Database Name</param>
+    /// <returns>SqlConnectionStringBuilder</returns>
+		public static SqlConnectionStringBuilder ConnectBase(string dataSource, string user, string pass,string db) {
+      SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+      builder.DataSource = dataSource;
+      builder.UserID = user;
+      builder.Password = pass;
+      builder.InitialCatalog = db;
+      
+      return builder;
+    }
+
+    /// <summary>
+    /// Return a DataTable filled from a sql select query.
+    /// </summary>
+    /// <param name="connection">connection name</param>
+    /// <param name="query">SQL Query</param>
+    /// <returns>DataTable</returns>
+    public static DataTable GetData(this SqlConnectionStringBuilder connection, string query) {
+      DataTable dt = new DataTable();
+      try {
+        using (SqlConnection con = new SqlConnection(connection.ConnectionString))
+        {
+          con.Open();
+          SqlCommand command = con.CreateCommand();
+          command.Connection = con;
+          command.CommandText = query;
+          SqlDataAdapter adapter = new SqlDataAdapter(command);
+          adapter.SelectCommand.CommandTimeout = 1200;
+          adapter.Fill(dt);
+        }
+      } catch(Exception e) {
+        Console.WriteLine(e.Message);
+			}
+      
+      return dt;
+		}
+
+    /// <summary>
+    /// This function execute insert,update and delete operations
+    /// </summary>
+    /// <param name="connection">connection name</param>
+    /// <param name="query">SQL Query</param>
+    /// <returns>int value with number of affected rows</returns>
+    public static int ModifyData(this SqlConnectionStringBuilder connection, string query) {
+      int affectedRows = 0;
+      try {
+        using (SqlConnection con = new SqlConnection(connection.ConnectionString))
+        {
+          con.Open();
+          SqlCommand command = con.CreateCommand();
+          command.Connection = con;
+          command.CommandText = query;
+          affectedRows = command.ExecuteNonQuery();
+        }
+      }
+      catch (Exception e)
+      {
+        Console.WriteLine(e.Message);
+      }
+
+      return affectedRows;
+		}
+	}
+}
